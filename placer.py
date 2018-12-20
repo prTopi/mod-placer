f9042fabe17bfa4e2495f839a1fb367536f40498
Topi Harjunpää <fintsh@gmail.com>
2018-12-20 15:24:43 +0200

    Only import needed functions

--- placer.py
+++ placer.py
@@ -1,16 +1,17 @@
 #!/bin/python
-import os, sys
+from sys import argv
+from os import listdir, system, path
 pluginsFile = '/home/topi/Games/Steam/steamapps/compatdata/72850/pfx/drive_c/users/steamuser/Local Settings/Application Data/Skyrim/plugins.txt'
 data = '/home/topi/Games/Steam/steamapps/common/Skyrim/Data/'
-folders = sorted(os.listdir('Mods'))
+folders = sorted(listdir('Mods'))
 mods = ','.join(sys.argv[1:])
 if mods.strip():
 	mods = [str(y) if len(str(y)) > 1 else '0'+str(y) for x in mods.split(',') for y in range(int(x.split('-')[0]), int(x.split('-')[-1])+1)]
 else:
 	mods = [x[:2] for x in folders if x[:2].isdigit()]
-os.system('rm -rf {}*'.format(data))
-[os.system('cp -as --remove-destination {!s}/Mods/{!s}-*/* {}'.format(os.path.dirname(os.path.realpath(__file__)), x, data)) for x in mods]
-files = os.listdir(data)
+system('rm -rf {}*'.format(data))
+[system('cp -as --remove-destination {!s}/Mods/{!s}-*/* {}'.format(path.dirname(path.realpath(__file__)), x, data)) for x in mods]
+files = listdir(data)
 with open(pluginsFile) as f:
 	plugins = f.read().splitlines()
 	files = [x for x in files if '.esp' in x or '.esm' in x]
