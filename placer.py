7cd8678d48aac480fe63ce0035cdd7e0d1e26ae4
Topi Harjunpää <fintsh@gmail.com>
2019-02-22 12:34:30 +0200

    Made mod info editing into 1 QDialog, instead of 3 QInputDialogs Paths are now run through path.realpath() Reordered buttons Added many failsafes, like checking if api exists, if data folder exists etc. "mods" folder is now created if it is missing Renamed variables selectSave() now uses shutil and custom function to do all of the symlinks instead of relying on rm, and cp Main window is now disabled when saving (Even though it should be close to instantaneous) and when editing mod info When renaming a mod, we now check if a mod with that name exists Added message if no mod updates are found

--- placer.py
+++ placer.py
@@ -1,14 +1,37 @@
 #!/bin/python
 from sys import argv
 from platform import platform, python_version
-from os import listdir, system, path, rename
+from os import listdir, path, rename, symlink, mkdir, unlink
+from shutil import rmtree
 from json import load
 from urllib.request import urlopen, Request
 from configparser import ConfigParser
-from PyQt5.QtWidgets import QApplication, QWidget, QListWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QAbstractItemView, QListWidgetItem, QInputDialog, QLineEdit, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QDialog, QDialogButtonBox, QListWidget, QListWidgetItem, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QAbstractItemView, QLineEdit, QMessageBox
 from PyQt5.QtCore import Qt
 
 __version__ = '0.1.0'
+
+class ModDialog(QDialog):
+	def __init__(self, name, modID, version, parent=None):
+		super().__init__(parent)
+		layout = QVBoxLayout(self)
+		self.nameEdit = QLineEdit(name, self)
+		self.idEdit = QLineEdit(modID, self)
+		self.versEdit = QLineEdit(version, self)
+		layout.addWidget(QLabel('Name', self))
+		layout.addWidget(self.nameEdit)
+		layout.addWidget(QLabel('Mod ID', self))
+		layout.addWidget(self.idEdit)
+		layout.addWidget(QLabel('Version', self))
+		layout.addWidget(self.versEdit)
+		buttons = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, Qt.Horizontal, self)
+		buttons.accepted.connect(self.accept)
+		buttons.rejected.connect(self.reject)
+		layout.addWidget(buttons)
+		self.show()
+
+	def getValues(self):
+		return self.nameEdit.text(), self.idEdit.text(), self.versEdit.text()
 
 class ModPlacer(QWidget):
 	def __init__(self):
@@ -16,50 +39,55 @@ class ModPlacer(QWidget):
 		self.config = ConfigParser()
 		self.config.read('config.ini')
 		self.headers = {'User-Agent': 'ModPlacer/{} ({}) Python/{}'.format(__version__, platform(), python_version()), 'apikey': self.config['Common']['api']}
+		self.config['Common']['data'] = path.realpath(self.config['Common']['data'])
+		self.config['Common']['plugins'] = path.realpath(self.config['Common']['plugins'])
 		self.folder = path.dirname(path.realpath(__file__))
 		self.initUI()
 
 	def initUI(self):
-		self.resize(750, 600)
+		self.resize(850, 700)
 		self.setWindowTitle('Mod Placer')
-		bRefresh = QPushButton('Refresh')
-		bRefresh.clicked.connect(self.refreshMods)
-		bUpdates = QPushButton('Check for updates')
-		bUpdates.clicked.connect(self.checkUpdates)
-		if not self.config['Common']['api']:
-			bUpdates.setEnabled(False)
 		modBox = QVBoxLayout()
 		self.modList = QListWidget(self)
 		self.modList.itemDoubleClicked.connect(lambda: self.changeModInfo(self.modList.currentItem()))
 		self.modList.setDragDropMode(QAbstractItemView.InternalMove)
-		self.modList.setSelectionMode(QAbstractItemView.SingleSelection)
 		[self.addModItem(*self.config['Mods'][mod].split('|')) for mod in self.config['Mods']]
 		modBox.addWidget(QLabel('Mods', self))
 		modBox.addWidget(self.modList)
 		loadBox = QVBoxLayout()
 		self.loadOrder = QListWidget(self)
 		self.loadOrder.setDragDropMode(QAbstractItemView.InternalMove)
-		self.loadOrder.setSelectionMode(QAbstractItemView.SingleSelection)
 		[self.addLoadItem(*self.config['LoadOrder'][esp].split('|')) for esp in self.config['LoadOrder']]
 		loadBox.addWidget(QLabel('Load Order', self))
 		loadBox.addWidget(self.loadOrder)
+		bRefresh = QPushButton('Refresh')
+		bRefresh.clicked.connect(self.refreshMods)
+		bUpdates = QPushButton('Check for updates')
+		bUpdates.clicked.connect(self.checkUpdates)
 		bSave = QPushButton('Save')
 		bSave.clicked.connect(self.selectSave)
 		topBox = QHBoxLayout()
-		topBox.addWidget(bUpdates)
-		topBox.addStretch(0)
-		topBox.addWidget(bRefresh)
-		midBox = QHBoxLayout()
-		midBox.addLayout(modBox)
-		midBox.addLayout(loadBox)
+		topBox.addLayout(modBox)
+		topBox.addLayout(loadBox)
 		botBox = QHBoxLayout()
+		botBox.addWidget(bRefresh)
+		botBox.addWidget(bUpdates)
 		botBox.addStretch(0)
 		botBox.addWidget(bSave)
 		layout = QVBoxLayout()
 		layout.addLayout(topBox)
-		layout.addLayout(midBox)
 		layout.addLayout(botBox)
 		self.setLayout(layout)
+		if not self.config['Common']['api']:
+			bUpdates.setEnabled(False)
+		if not path.isdir(self.config['Common']['data']):
+			QMessageBox.critical(self, 'Error', 'Data folder not found. ({})'.format(self.config['Common']['data']), QMessageBox.Ok)
+			bRefresh.setEnabled(False)
+			bSave.setEnabled(False)
+		if not path.isdir(self.config['Common']['plugins']):
+			QMessageBox.critical(self, 'Error', 'Plugins config folder not found. ({})'.format(self.config['Common']['data']), QMessageBox.Ok)
+		if not path.isdir(path.join(self.folder, 'mods')):
+			mkdir(path.join(self.folder, 'mods'))
 		self.refreshMods()
 		self.show()
 
@@ -70,50 +98,70 @@ class ModPlacer(QWidget):
 		[self.addModItem(mod) for mod in listdir('mods')]
 		self.loadOrder.clear()
 		[self.addLoadItem(*self.config['LoadOrder'][esp].split('|')) for esp in self.config['LoadOrder']]
-		[self.addLoadItem(esp) for esp in listdir(path.join(self.config['Common']['Data'])) if esp.endswith(('.esm', '.esp', '.esl'))]
+		[self.addLoadItem(esp) for esp in listdir(path.join(self.config['Common']['data'])) if esp.endswith(('.esm', '.esp', '.esl'))]
 
 	def changeModInfo(self, item):
+		self.setEnabled(False)
 		iData = item.data(Qt.UserRole).split('|')
-		name, ok = QInputDialog(self).getText(self, 'Edit', 'Name', QLineEdit.Normal, item.text())
-		if ok:
-			modId, ok = QInputDialog(self).getText(self, 'Edit', 'ID', QLineEdit.Normal, iData[0])
-			if ok:
-				version, ok = QInputDialog(self).getText(self, 'Edit', 'Version', QLineEdit.Normal, iData[1])
-				if ok:
+		self.dialog = ModDialog(item.text(), iData[0], iData[1], self)
+		if self.dialog.exec_():
+			name, modID, version = self.dialog.getValues()
+			if name != item.text():
+				if name in listdir(path.join(self.folder, 'mods')):
+					QMessageBox.warning(self, 'Warning', 'Mod folder with that name already exists.', QMessageBox.Ok)
+				else:
 					rename(path.join(self.folder, 'mods', item.text()), path.join(self.folder, 'mods', name))
 					item.setText(name)
-					self.updateData(item, modId, version)
+			self.updateData(item, modID, version)
+		self.setEnabled(True)
 
-	def updateData(self, item, modId, version):
-		item.setToolTip('ID: {}\nVersion: {}'.format(modId, version))
-		item.setData(Qt.UserRole, '{}|{}'.format(modId, version))
+	def updateData(self, item, modID, version):
+		item.setToolTip('ID: {}\nVersion: {}'.format(modID, version))
+		item.setData(Qt.UserRole, '{}|{}'.format(modID, version))
 
-	def addModItem(self, mod, check=Qt.Unchecked, modId='0', version='1.0'):
+	def addModItem(self, mod, check=Qt.Unchecked, modID='0', version='1.0'):
 		if path.isdir(path.join(self.folder, 'mods', mod)):
 			if not self.modList.findItems(mod, Qt.MatchExactly):
 				modItem = QListWidgetItem(mod)
 				modItem.setData(Qt.CheckStateRole, check)
-				self.updateData(modItem, modId, version)
+				self.updateData(modItem, modID, version)
 				self.modList.addItem(modItem)
 
 	def addLoadItem(self, esp, check=Qt.Unchecked):
-		if path.isfile(path.join(self.config['Common']['Data'], esp)):
+		if path.isfile(path.join(self.config['Common']['data'], esp)):
 			if not self.loadOrder.findItems(esp, Qt.MatchExactly):
 				loadItem = QListWidgetItem(esp)
 				loadItem.setData(Qt.CheckStateRole, check)
 				self.loadOrder.addItem(loadItem)
 
 	def selectSave(self):
+		self.setEnabled(False)
 		self.saveConfig()
-		system('rm -rf "{}/"*'.format(self.config['Common']['Data']))
-		[system('cp -as --remove-destination "{!s}/"* "{}"'.format(path.join(self.folder, 'mods', self.modList.item(index).text()), self.config['Common']['Data'])) for index in range(self.modList.count()) if self.modList.item(index).checkState()]
+		rmtree(self.config['Common']['data'])
+		mkdir(self.config['Common']['data'])
+		[self.linktree(path.join(self.modList.item(index).text()), self.config['Common']['data']) for index in range(self.modList.count()) if self.modList.item(index).checkState()]
 		self.refreshMods()
+		self.setEnabled(True)
+
+	def linktree(self, source, destination):
+		for item in listdir(path.join(self.folder, 'mods', source)):
+			src = path.join(self.folder, 'mods', source, item)
+			dst = path.join(destination, item)
+			if path.isdir(src):
+				if not path.isdir(dst):
+					mkdir(dst)
+				self.linktree(src, dst)
+			else:
+				if path.islink(dst):
+					unlink(dst)
+				symlink(src, dst)
 
 	def saveConfig(self):
 		self.config['Mods'] = {x: '|'.join([self.modList.item(x).text(), str(self.modList.item(x).checkState()), self.modList.item(x).data(Qt.UserRole)]) for x in range(self.modList.count())}
 		self.config['LoadOrder'] = {x: '|'.join([self.loadOrder.item(x).text(), str(self.loadOrder.item(x).checkState())]) for x in range(self.loadOrder.count())}
-		with open(path.join(self.config['Common']['Plugins'], 'Plugins.txt'), 'w') as pFile:
-			[pFile.write('*{}\n'.format(self.loadOrder.item(index).text())) for index in range(self.loadOrder.count()) if self.loadOrder.item(index).checkState()]
+		if path.isdir(self.config['Common']['plugins']):
+			with open(path.join(self.config['Common']['plugins'], 'Plugins.txt'), 'w') as pFile:
+				[pFile.write('*{}\n'.format(self.loadOrder.item(index).text())) for index in range(self.loadOrder.count()) if self.loadOrder.item(index).checkState()]
 		with open('config.ini', 'w') as cFile:
 			self.config.write(cFile)
 
@@ -132,6 +180,8 @@ class ModPlacer(QWidget):
 				if modData[1] != version:
 					updates += '\n{}: {} --> {}'.format(mod.text(), modData[1], version)
 		self.setEnabled(True)
+		if not updates:
+			updates = 'No mod updates found.'
 		QMessageBox.information(self, 'Mod updates', 'Mod updates:\n' + updates, QMessageBox.Ok)
 
 	def closeEvent(self, event):
