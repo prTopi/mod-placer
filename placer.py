668303d34842010bc0e69ddfe31e1a15ccd5f12d
Topi Harjunpää <topi@harjunpaa.fi>
2019-07-02 11:33:10 +0300

    Added simple warning if Python version is too old

--- placer.py
+++ placer.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
-from sys import argv, exit
-from PyQt5.QtWidgets import QApplication
+from sys import argv, exit, version_info
+from PyQt5.QtWidgets import QApplication, QMessageBox
 from placer.application import ModPlacer
 
 
 if __name__ == "__main__":
     app = QApplication(argv)
-    window = ModPlacer()
-    exit(app.exec_())
+    if version_info >= (3, 6):
+        window = ModPlacer()
+        exit(app.exec_())
+    else:
+        QMessageBox.critical(None, "Python update needed",
+                             "Only Python versions above 3.6 are supported.\n"
+                             "Please update your Python version.",
+                             QMessageBox.Close)
